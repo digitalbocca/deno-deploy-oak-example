99ed0b357edcc62e1acf493481d423437f694b1f
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2021-09-04 00:01:11 -0300

    Cria roteamento

--- src/mod.ts
+++ src/mod.ts
@@ -1,9 +1,14 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts'
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
+
+const router = new Router()
+
+router.get(ctx => {
+  ctx.response.body = 'Hello world!'
+})
 
 const app = new Application()
 
-app.use((ctx) => {
-  ctx.response.body = 'Hello world!'
-})
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 await app.listen({ port: Deno.env.get('PORT') })
